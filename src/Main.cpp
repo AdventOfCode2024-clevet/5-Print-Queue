bfcdad6d4ebf1a7697ac2b67115b5bb9eae5425f
corentinlevet <corentin.levet@epitech.eu>
2024-12-05 12:28:59 +0100

    Part two solution.

--- src/Main.cpp
+++ src/Main.cpp
@@ -87,25 +87,18 @@ int main(int argc, char* argv[]) {
         update_states.push_back(std::make_pair(valid, update));
     }
 
-    // Debug
-    // Cycle through the update states and print the valid state and the vector
-    for (const auto &update_state : update_states)
-    {
-        std::cout << "Valid: " << update_state.first << " ";
-        for (const auto &update : update_state.second)
-        {
-            std::cout << update << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    // We now make a new array with only the valid states
+    // We now make a new array with only the valid states, and one with the invalid states
     std::vector<std::vector<int>> valid_updates;
+    std::vector<std::vector<int>> invalid_updates;
     for (const auto &update_state : update_states)
     {
         if (update_state.first)
         {
             valid_updates.push_back(update_state.second);
+        }
+        else
+        {
+            invalid_updates.push_back(update_state.second);
         }
     }
 
@@ -117,7 +110,39 @@ int main(int argc, char* argv[]) {
         result += middle;
     }
 
-    std::cout << "Result: " << result << std::endl;
+    std::cout << "Result (part 1): " << result << std::endl;
+
+    // Part two
+    // First, we need to order correctly the invalid updates following the rules
+
+    std::vector<std::vector<int>> ordered_invalid_updates;
+    for (const auto &invalid_update : invalid_updates) {
+        std::vector<int> ordered_update = invalid_update;
+        bool changed;
+        do {
+            changed = false;
+            for (int i = 0; i < ordered_update.size() - 1; i++) {
+                for (const auto &rule : rules) {
+                    if (ordered_update[i] == rule.first && ordered_update[i + 1] == rule.second) {
+                        std::swap(ordered_update[i], ordered_update[i + 1]);
+                        changed = true;
+                    }
+                }
+            }
+        } while (changed);
+        // We need to reverse the order of the update afterwards to have the correct order
+        std::reverse(ordered_update.begin(), ordered_update.end());
+        ordered_invalid_updates.push_back(ordered_update);
+    }
+
+    // Same as before, we need to add the middle number of each invalid update to the result
+    result = 0;
+    for (const auto &ordered_invalid_update : ordered_invalid_updates) {
+        int middle = ordered_invalid_update[ordered_invalid_update.size() / 2];
+        result += middle;
+    }
+
+    std::cout << "Result (part 2): " << result << std::endl;
 
     return (0);
 }
